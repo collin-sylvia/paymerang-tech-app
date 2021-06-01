6c8afff49c37cf590370401ee2fe58d60eb5dded
collin-sylvia <collincsylvia@gmail.com>
2021-06-01 16:26:22 -0400

    deploying

--- paymerang-app/server.js
+++ paymerang-app/server.js
@@ -1,19 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
-app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
